a6444a926e866fe8802e4677d7227c09d4e0f9bc
Viktor Bereziouk <victorbereziouk@gmail.com>
2021-12-27 17:03:29 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-function compute()
 {   
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
@@ -11,7 +6,7 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" + "at an interest rate of <mark>" + rate + "</mark>,<br/>" + "You will receive an amount of <mark>" + interest + "</mark>,<br/> " + "in the year <mark>" + years + "</mark>";
 }
         
 function updateRate() {
