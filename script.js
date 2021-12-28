52d7c00d435ebaaa15cc6769d1242f5187d2f9dd
Viktor Bereziouk <victorbereziouk@yahoo.com>
2021-12-28 01:26:33 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     
-    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" + "at an interest rate of <mark>" + rate + "</mark>,<br/>" + "You will receive an amount of <mark>" + interest + "</mark>,<br/> " + "in the year <mark>" + years + "</mark>";
+    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" + "at an interest rate of <mark>" + rate + "</mark>,<br/>" + "You will receive an amount of <mark>" + interest + "</mark>,<br/> " + "in the year <mark>" + year + "</mark>";
 }
         
 function updateRate() {
@@ -14,9 +14,10 @@ function updateRate() {
     document.getElementById("rate_val").innerText=rateval;
 }
 
-/* function verify() {
-var principal = document.getElementById("principal").value
-if (principal == <0) {
-    alert("Enter a positive number");
-    document.getElementById("principal").focus();
-    } */
+function verify() {
+    var principal = document.getElementById("principal").value
+    if (principal <=0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        }
+    }
